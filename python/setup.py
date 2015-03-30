0200c160950e4028f799a98b865783f1d541c058
Alessio Magro <lessju@gmail.com>
2015-03-30 09:33:16 +0100

    Added simple install scripts

---
+++ python/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+# Setup information (basic setup)
+setup(name='TPM',
+      version='0.2',
+      description='Python wrapper for FPGA-board access layer',
+      author='Alessio Magro',
+      py_modules=['tpm'],
+     )
